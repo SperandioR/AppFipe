01ea33f26e5c80628792062718a36881caf0516d
Rodrigo Sperandio <rodrigopaulino13@gmail.com>
2022-10-29 12:24:34 -0300

    criando pasta HttpClient

---
+++ AppFipe/Util/HttpClientUtil.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppFipe.Util
+{
+    public class HttpClientUtil // Alterando a classe interno para public.
+    {
+
+
+    }
+}
