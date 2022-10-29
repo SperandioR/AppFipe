3ebb02e5b5f629d76b385dbd98950b68608c7523
Rodrigo Sperandio <rodrigopaulino13@gmail.com>
2022-10-29 14:04:18 -0300

    requisição Http para buscar Url.

--- AppFipe/Util/HttpClientUtil.cs
+++ AppFipe/Util/HttpClientUtil.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System; //Biblioteca C#.
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +8,28 @@ namespace AppFipe.Util
 {
     public class HttpClientUtil // Alterando a classe interno para public.
     {
+        //Criando Métodos com nome Http... e adicionando endereço.
+        public static async Task<String> ConsHttpClientAsync(String endereco)
+        {
+            
+           //criando uma requisição Http para buscar uma requsição da Url.
+           var client = new HttpClient();               
+            try
+            {
+                HttpRequestMessage response = await client.GetAsync(endereco).ConfigureAwait(false);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
 
+                return responseBody;
+
+            }
+            catch (Exception)
+            {
+                return "";
+                
+            }
+
+        }
 
     }
 }
