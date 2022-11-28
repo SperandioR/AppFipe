bd4014413f383d4ac55c2c5e2d4c95af3b82a539
Rodrigo Sperandio <rodrigopaulino13@gmail.com>
2022-11-28 20:45:42 -0300

    criando uma classe modelo dos Fabricantes.

---
+++ AppFipe/Models/modeloFabricante.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppFipe.Models
+{
+    public class modeloFabricante
+    {
+        public int MyProperty { get; set; }
+
+    }
+}
